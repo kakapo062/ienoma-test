2fc660d1a6d170ebd2bcc5858c431422be900e51
kakapo062 <61102115+kakapo062@users.noreply.github.com>
2022-03-07 16:19:50 +0900

    がっつり改修

---
+++ ibaraki/thanks.php
@@ -0,0 +1,17 @@
+<?php include ( dirname(__FILE__) . '/../header.php' ); ?>
+
+<main class="thanks_main">
+    <div class="thanks_wrap">
+        <div class="thanks_inner">
+            <h2 class="thanks_title">お申し込みが完了しました。</h2>
+            <p class="thanks_para">家づくり相談のお申込みありがとうございました。</p>
+            <p class="thanks_para">内容を確認の上、担当者からご連絡させていただきます。</p>
+            <p class="thanks_para">なお、しばらくたっても連絡がない場合は、
+                <br>お客様によりご入力いただいた情報に誤りがある場合がございます。
+            </p>
+            <p class="thanks_para">その際は、お手数ですが再度お問い合わせいただけますと幸いです。</p>
+        </div>
+    </div>
+</main>
+
+<?php include ( dirname(__FILE__) . '/../footer.php' ); ?>
