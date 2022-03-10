4a16b24ee0371b63ae6e3fb691591a7e1c15a9a1
kakapo062 <61102115+kakapo062@users.noreply.github.com>
2022-03-10 15:51:45 +0900

    最終改修

--- ibaraki/thanks.php
+++ ibaraki/thanks.php
@@ -6,37 +6,95 @@ $token = isset($_POST["token"]) ? $_POST["token"] : "";
 $session_token = isset($_SESSION["token"]) ? $_SESSION["token"] : "";
 // セッション変数のトークンを削除
 unset($_SESSION["token"]);
-// POSTされたトークンとセッション変数のトークンの比較
-if($token != "" && $token == $session_token) {
-// フォーム送信実行
+
 //エスケープ処理やデータチェックを行う関数のファイルの読み込み
 require 'libs/functions.php';
+
+//mbstringの日本語設定
+mb_language( 'ja' );
+mb_internal_encoding( 'UTF-8' );
+
 
 //POSTされたデータがあれば変数に格納、なければ NULL（変数の初期化）
 $name = isset( $_POST[ 'name' ] ) ? $_POST[ 'name' ] : NULL;
 $email = isset( $_POST[ 'email' ] ) ? $_POST[ 'email' ] : NULL;
 $phone = isset( $_POST[ 'phone' ] ) ? $_POST[ 'phone' ] : NULL;
 
+  //POSTされたデータに不正な値がないかを別途定義した checkInput() 関数で検証
+  $_POST = checkInput( $_POST );
+  //filter_var を使って値をフィルタリング
+  if(isset($_POST['email'])) {
+    //全ての改行文字を削除
+    $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+    //E-mail の形式にフィルタ
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+  }
+  if(isset($_POST['phone'])) {
+    //数値の形式にフィルタ（数字、+ 、- 記号 以外を除去）
+    $phone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
+  }
 
-//送信ボタンが押された場合の処理
-if (isset($_POST['submitted'])) {
-    //POSTされたデータに不正な値がないかを別途定義した checkInput() 関数で検証
-    $_POST = checkInput( $_POST );
-    //filter_var を使って値をフィルタリング
-    if(isset($_POST['email'])) {
-      //全ての改行文字を削除
-      $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-      //E-mail の形式にフィルタ
-      $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-    if(isset($_POST['phone'])) {
-      //数値の形式にフィルタ（数字、+ 、- 記号 以外を除去）
-      $phone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
-    }
+try {
+
+  // POSTされたトークンとセッション変数のトークンの比較
+  if($token != "" && $token == $session_token) {
+
+    //送信ボタンが押された場合の処理
+    if (isset($_POST['submitted'])) {
+      //----------googleスプレッドシート送信---------------------
+  //composerでインストールしたライブラリを読み込む
+// require_once __DIR__ . '../../../../../vendor/autoload.php';
+require('../../../../vendor/autoload.php');
+// サービスアカウント認証で作成したjsonファイル.
+$key_file = __DIR__.'/../../../../key/ienoma-0f02b8344520.json';
+  // 対象のスプレッドシートのIDを指定
+$sheet_id = '133Yefgmmok5ERkWfLplOAKgvh7a-IqwwqODEgF2WGXo';
+//アカウント認証インスタンスの生成
+$client = new Google_Client();
+//サービスキーをセット
+$client->setAuthConfig($key_file);
+$client->setApplicationName('Sheet Api Test');
+//スコープを以下の内容でセット
+$scopes = [Google_Service_Sheets::SPREADSHEETS];
+$client->setScopes($scopes);
+
+  // シートの書き込みインスタンスを生成
+$sheet = new Google_Service_Sheets($client);
+
+try {
+  // シート名
+  $range = 'フォーム申込';
+  $values = array(
+      array(
+//連携したスプレッドシートの最終行に「A列（一番左）」から順に追記される
+          date('Y-m-d H:i:s'),////問い合わせ日時 2019-12-31 23:59:59
+          $name,//★フォームの入力値の取得
+          $phone,
+          $email,
+      ),
+  );
+
+  $appendBody = new Google_Service_Sheets_ValueRange([
+      'values' => $values
+  ]);
+// 書き込みの実行
+  $response = $sheet->spreadsheets_values->append(
+    $sheet_id,
+    $range,
+    $appendBody,
+    ['valueInputOption' => 'USER_ENTERED']
+  );
+} catch (Google_Exception $e) {
+  $errors = json_decode($e->getMessage(), true);
+  $err = "code : " . $errors["error"]["code"] . "\r\n";
+  $err .= "message : " . $errors["error"]["message"];
+  echo "Google_Exception" . $err;
+}
+
+// メール送信の内容
     //POST でのリクエストの場合
     if ($_SERVER['REQUEST_METHOD']==='POST') {
       // //メールアドレス等を記述したファイルの読み込み
-      // require 'libs/mailvars.php';
       //メール本文の組み立て。値は h() でエスケープ処理
       $mail_body = "イエノマLPから、" . "\n" . "以下の内容にてお申し込みがありました。" . "\n";
       $mail_body .= "*****************************" . "\n";
@@ -65,18 +123,16 @@ if (isset($_POST['submitted'])) {
       // $mailTo = mb_encode_mimeheader(MAIL_TO_NAME) ."<" . MAIL_TO. ">";
 
       // 宛先
-      $to = "ienoma@t-wind.co.jp";
+      $mailTo = "ienoma@t-wind.co.jp";
       // 件名
       $subject = "【イエノマLP】お申し込みがありました";
       // 送信元
-      $from = "イエノマ <k.kanazono@osen.co.jp>";
+      $from = "イエノマ <ienoma@t-wind.co.jp>";
       // 送信元メールアドレス
-      $from_mail = "k.kanazono@osen.co.jp";
+      $from_mail = "ienoma@t-wind.co.jp";
       // 送信者名
       $from_name = "イエノマ";
-      //mbstringの日本語設定
-      mb_language( 'ja' );
-      mb_internal_encoding( 'UTF-8' );
+
   
       // 送信者情報（From ヘッダー）の設定
       // $headers = "From: ienoma@t-wind.co.jp";
@@ -92,8 +148,7 @@ if (isset($_POST['submitted'])) {
       $headers .= "X-Priority: 3 \r\n";
   
       //メールの送信結果を変数に代入
-      // $result = mb_send_mail( $mailTo, $subject, $mail_body, $headers );
-      $result = mb_send_mail( $to, $subject, $mail_body, $headers );
+      $result = mb_send_mail( $mailTo, $subject, $mail_body, $headers );
       //メールが送信された場合の処理
       if ( $result ) {
          
@@ -139,57 +194,9 @@ if (isset($_POST['submitted'])) {
   }
   $url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']; 
   header('Location:' . $url . $params);
-                          //----------googleスプレッドシート送信---------------------
-  //composerでインストールしたライブラリを読み込む
-// require_once __DIR__ . '../../../../../vendor/autoload.php';
-require('../../../../vendor/autoload.php');
-// サービスアカウント認証で作成したjsonファイル.
-$key_file = __DIR__.'/../../../../key/ienoma-0f02b8344520.json';
-  // 対象のスプレッドシートのIDを指定
-$sheet_id = '133Yefgmmok5ERkWfLplOAKgvh7a-IqwwqODEgF2WGXo';
-//アカウント認証インスタンスの生成
-$client = new Google_Client();
-//サービスキーをセット
-$client->setAuthConfig($key_file);
-$client->setApplicationName('Sheet Api Test');
-//スコープを以下の内容でセット
-$scopes = [Google_Service_Sheets::SPREADSHEETS];
-$client->setScopes($scopes);
-
-  // シートの書き込みインスタンスを生成
-$sheet = new Google_Service_Sheets($client);
-
-try {
-  $range = 'フォーム申込';
-  $values = array(
-      array(
-//連携したスプレッドシートの最終行に「A列（一番左）」から順に追記される
-          date('Y-m-d H:i:s'),////問い合わせ日時 2019-12-31 23:59:59
-          $name,//★フォームの入力値の取得
-          $phone,
-          $email,
-      ),
-  );
-
-  $appendBody = new Google_Service_Sheets_ValueRange([
-      'values' => $values
-  ]);
-// 書き込みの実行
-  $response = $sheet->spreadsheets_values->append(
-    $sheet_id,
-    $range,
-    $appendBody,
-    ['valueInputOption' => 'USER_ENTERED']
-  );
-} catch (Google_Exception $e) {
-  $errors = json_decode($e->getMessage(), true);
-  $err = "code : " . $errors["error"]["code"] . "\r\n";
-  $err .= "message : " . $errors["error"]["message"];
-  echo "Google_Exception" . $err;
-}
         //空の配列を代入し、すべてのPOST変数を消去
         $_POST = array();
-  
+
         //変数の値も初期化
         $name = '';
         $email = '';
@@ -198,6 +205,10 @@ try {
     }
 }
 } else {
+}
+} catch (Exception $e) {
+  // エラーの場合
+  echo '送信に失敗しました: ', $mail->ErrorInfo;
 }
 ?>
 
